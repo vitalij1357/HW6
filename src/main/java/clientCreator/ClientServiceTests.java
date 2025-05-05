6ef1cfcd7d044d1c4668e21ff552fa69ca140888
vitalij1357 <vitaliyyankovyy9@gmail.com>
2025-05-06 00:01:51 +0200

    Fix

--- src/main/java/clientCreator/ClientServiceTests.java
+++ src/main/java/clientCreator/ClientServiceTests.java
@@ -6,18 +6,13 @@ import util.Client;
 import java.sql.SQLException;
 import java.util.List;
 
-public class testClientService {
+public class ClientServiceTests {
     public static void main(String[] args) throws SQLException {
-        ClientService clientService = new ClientService();
         DatabaseInitService databaseInitService = new DatabaseInitService();
-//        databaseInitService.initDb();
-//        clientService.create("Mark");
-//        clientService.deleteById(9);
-//     clientService.setName(10,"Igor");
-//       clientService.getById(10);
+        ClientDaoService clientDaoService = new ClientDaoService();
+        ClientService clientService = new ClientService(clientDaoService);
         List<Client> clients = clientService.listAll();
         clients.forEach(System.out::println);
-
 
     }
 }
